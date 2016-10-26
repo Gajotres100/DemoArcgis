9854f1346d59f0ce671dbb215e5e9685af650c77
ngaic <ngaic@DEVBASICHDD>
2016-10-26 09:48:30 +0200

    -

--- DemoArcgis/Controllers/HomeController.cs
+++ DemoArcgis/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace DemoArcgis.Controllers
+namespace Arcgis.Directions.UI.Controllers
 {
     public class HomeController : Controller
     {
