72191aad0b19829bd516129ca1974c9ccf49b7c4
Tekut <Tekut@DESKTOP-BLDJ8FL>
2017-02-13 19:44:15 +0100

    Dodavnje bundlinga

--- Arcgis.Directions.UI/Global.asax.cs
+++ Arcgis.Directions.UI/Global.asax.cs
@@ -1,27 +1,25 @@
-﻿using log4net;
+﻿using Arcgis.Utils.Web;
+using log4net;
 using System;
 using System.ComponentModel;
 using System.Reflection;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Arcgis.Directions.UI
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        private static Container _container;
         private static readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-        public static Container InjectorContainer
-        {
-            get { return _container; }
-            private set { _container = value; }
-        }
+        public static Container InjectorContainer { get; set; }
 
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
         protected void Application_Error(object sender, EventArgs e)
---
+++ Arcgis.Utils/Web/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Optimization;
+
+namespace Arcgis.Utils.Web
+{
+    public class BundleConfig
+    {
+        /// <summary>
+        /// Registers bundles with the application.
+        /// </summary>
+        /// <param name="bundles">The bundles to register.</param>
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/*.css"));
+        }
+    }
+}
