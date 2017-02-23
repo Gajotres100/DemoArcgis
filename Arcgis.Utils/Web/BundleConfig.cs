4fb6834a36c94721151b22463642362ee03627d4
dprten <nino00700@gmail.com>
2017-02-23 23:02:54 +0000

    --

--- Arcgis.Utils/Web/BundleConfig.cs
+++ Arcgis.Utils/Web/BundleConfig.cs
@@ -8,36 +8,40 @@ namespace Arcgis.Utils.Web
         {
             #region Template 1
 
-            bundles.Add(new StyleBundle("~/Content/1/css/").Include(                                        
+            bundles.Add(new StyleBundle("~/Content/1/css/").Include(
                                         "~/Content/1/css/esri.css",
                                         "~/Content/1/css/style.css",
-                                        "~/assets/plugins/bootstrap/css/bootstrap.min.css",
-                                        "~/assets/plugins/bootstrap/css/bootstrap-responsive.min.css",
-                                        "~/assets/plugins/font-awesome/css/font-awesome.min.css",
-                                        "~/assets/css/style-metro.css",
-                                        "~/assets/css/style.css",
-                                        "~/assets/css/style-responsive.css",
-                                        "~/assets/css/themes/default.css",
-                                        "~/assets/plugins/uniform/css/uniform.default.css",
-                                        "~/assets/plugins/bootstrap-tag/css/bootstrap-tag.css",
-                                        "~/assets/plugins/bootstrap-wysihtml5/bootstrap-wysihtml5.css",
-                                        "~/assets/plugins/fancybox/source/jquery.fancybox.css",
-                                        "~/assets/plugins/bootstrap-wysihtml5/bootstrap-wysihtml5.css",
-                                        "~/assets/plugins/jquery-file-upload/css/jquery.fileupload-ui.css",
-                                        "~/assets/css/pages/inbox.css"
+                                        "~/Content/1/css/style-metro.css",
+                                        "~/Content/1/css/style-responsive.css",
+                                        "~/Content/1/css/themes/default.css",
+                                        "~/Content/1/css/pages/inbox.css"
                                         ));
+            bundles.Add(new StyleBundle("~/Content/1/plugins/").Include(
+                "~/Content/1/plugins/bootstrap/css/bootstrap.min.css",
+                                        "~/Content/1/plugins/bootstrap/css/bootstrap-responsive.min.css",
+                                        "~/Content/1/plugins/font-awesome/css/font-awesome.min.css",
+                                         "~/Content/1/plugins/uniform/css/uniform.default.css",
+                                        "~/Content/1/plugins/bootstrap-tag/css/bootstrap-tag.css",
+                                        "~/Content/1/plugins/bootstrap-wysihtml5/bootstrap-wysihtml5.css",
+                                        "~/Content/1/plugins/fancybox/source/jquery.fancybox.css",
+                                        "~/Content/1/plugins/bootstrap-wysihtml5/bootstrap-wysihtml5.css",
+                                        "~/Content/1/plugins/jquery-file-upload/css/jquery.fileupload-ui.css"
+                                        ));
+            bundles.Add(new ScriptBundle("~/Content/1/scripts/").Include(
+                             "~/Content/1/scripts/app.js"
+                            ));
+            bundles.Add(new ScriptBundle("~/Content/1/plugins/").Include(
+                            "~/Content/1/plugins/breakpoints/breakpoints.js",
+                            "~/Content/1/plugins/jquery-slimscroll/jquery.slimscroll.min.js",
+                            "~/Content/1/plugins/jquery.blockui.js",
+                            "~/Content/1/plugins/jquery.cookie.js",
+                            "~/Content/1/plugins/uniform/jquery.uniform.min.js",
+                            "~/Content/1/plugins/bootstrap-tag/js/bootstrap-tag.js",
+                            "~/Content/1/plugins/fancybox/source/jquery.fancybox.pack.js",
+                            "~/Content/1/plugins/bootstrap-wysihtml5/wysihtml5-0.3.0.js",
+                            "~/Content/1/plugins/bootstrap-wysihtml5/bootstrap-wysihtml5.js",
+                            "~/Content/1/plugins/bootstrap/css/bootstrap.min.css"
 
-            bundles.Add(new ScriptBundle("~/assets/scripts/").Include(
-                            "~/assets/plugins/breakpoints/breakpoints.js",
-                            "~/assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js",
-                            "~/assets/plugins/jquery.blockui.js",
-                            "~/assets/plugins/jquery.cookie.js",
-                            "~/assets/plugins/uniform/jquery.uniform.min.js",
-                            "~/assets/plugins/bootstrap-tag/js/bootstrap-tag.js",
-                            "~/assets/plugins/fancybox/source/jquery.fancybox.pack.js",
-                            "~/assets/plugins/bootstrap-wysihtml5/wysihtml5-0.3.0.js",
-                            "~/assets/plugins/bootstrap-wysihtml5/bootstrap-wysihtml5.js",
-                            "~/assets/scripts/app.js"
                             ));
 
             #endregion
@@ -46,8 +50,46 @@ namespace Arcgis.Utils.Web
 
             bundles.Add(new StyleBundle("~/Content/2/css/").Include(
                             "~/Content/2/css/esri.css",
-                            "~/Content/2/css/style.css"
+                            "~/Content/2/css/style.css",
+                            "~/Content/2/css/style-metro.css",
+                            "~/Content/2/css/style-responsive.css",
+                            "~/Content/2/css/pages/inbox.css"
+
                             ));
+
+            bundles.Add(new ScriptBundle("~/Content/2/plugins/").Include(
+                           "~/Content/2/plugins/breakpoints/breakpoints.js",
+                           "~/Content/2/plugins/jquery-slimscroll/jquery.slimscroll.min.js",
+                           "~/Content/2/plugins/jquery.blockui.js",
+                           "~/Content/2/plugins/jquery.cookie.js",
+                           "~/Content/2/plugins/uniform/jquery.uniform.min.js",
+                           "~/Content/2/plugins/bootstrap-tag/js/bootstrap-tag.js",
+                           "~/Content/2/plugins/fancybox/source/jquery.fancybox.pack.js",
+                           "~/Content/2/plugins/bootstrap-wysihtml5/wysihtml5-0.3.0.js",
+                           "~/Content/2/plugins/bootstrap-wysihtml5/bootstrap-wysihtml5.js",
+                           "~/Content/2/plugins/bootstrap/css/bootstrap.min.css",
+                           "~/Content/2/plugins/bootstrap/css/bootstrap-responsive.min.css",
+                           "~/Content/2/plugins/font-awesome/css/font-awesome.min.css",
+                           "~/Content/2/plugins/uniform/css/uniform.default.css",
+                           "~/Content/2/plugins/bootstrap-tag/css/bootstrap-tag.css",
+                           "~/Content/2/plugins/fancybox/source/jquery.fancybox.css",
+                           "~/Content/2/plugins/jquery-file-upload/css/jquery.fileupload-ui.css",
+                           "~/Content/2/plugins/bootstrap/css/bootstrap.min.css"
+
+                           ));
+
+
+            bundles.Add(new ScriptBundle("~/Content/2/scripts/").Include(
+                           "~/Content/2/scripts/app.js",
+                           "~/Content/2/scripts/menu-action.js"
+                           ));
+
+
+
+
+
+
+
 
             #endregion
 
