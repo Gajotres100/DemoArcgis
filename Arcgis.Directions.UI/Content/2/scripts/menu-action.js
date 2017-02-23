ccce77db01d3d084392f3037f01c03f373c8b52e
Tekut <Tekut@DESKTOP-BLDJ8FL>
2017-02-23 19:52:29 +0100

    -

---
+++ Arcgis.Directions.UI/Content/2/scripts/menu-action.js
@@ -0,0 +1,15 @@
+﻿
+$(document).ready(function () {
+    $(".menu-toggle").click(function () {
+        $('#sidebar li').removeClass();
+        
+        $('.menu-toggle').hide();
+
+    });
+    $("ul.main-menu li").click(function (event) {
+        $('#sidebar li').removeClass('selected');
+        $(this).toggleClass('selected');
+        $('.menu-toggle').show();
+    });
+
+});
